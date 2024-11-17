a6ffb2bb61ec0dc2bea7839da61a2374ee7931f0
mFefasDev <40470600+mfeeee@users.noreply.github.com>
2024-11-16 22:53:49 -0300

    Sexto Commit

---
+++ hellodjango.py
@@ -0,0 +1,11 @@
+def ola(nome):
+    print('Ola ' + nome + '!')
+
+
+garotas = ['Maria', 'Fernanda', 'Livia', 'Vitoria', 'Miranda']
+for nome in garotas:
+    ('Proxima')
+
+for i in range(1,7):
+    print(i)
+

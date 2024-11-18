28463659a4279fcbc1503c648a0f4de09a0734bf
mFefasDev <40470600+mfeeee@users.noreply.github.com>
2024-11-18 12:41:33 -0300

    Organizando a views.py DecimoPrimeiro Commit

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
-from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
-
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def post_new(request):
